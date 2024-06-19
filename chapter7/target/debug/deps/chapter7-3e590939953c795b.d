67c38d3f0a4db0b0cdfe7ca321281ee533fa0c18
seop02 <useop02@gmail.com>
2024-06-19 18:23:56 +0100

    test

---
+++ chapter7/target/debug/deps/chapter7-3e590939953c795b.d
@@ -0,0 +1,7 @@
+/Users/wooseophwang/rust_tutorial/chapter7/target/debug/deps/chapter7-3e590939953c795b: src/main.rs src/garden.rs src/garden/vegetables.rs
+
+/Users/wooseophwang/rust_tutorial/chapter7/target/debug/deps/chapter7-3e590939953c795b.d: src/main.rs src/garden.rs src/garden/vegetables.rs
+
+src/main.rs:
+src/garden.rs:
+src/garden/vegetables.rs:
